693e2a6cbc40365222dcde00cd41b2cc53e82610
Jaco-Miras <58285015+Jaco-Miras@users.noreply.github.com>
2023-01-09 14:38:15 +0800

    sending realtime chat to messages

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,10 @@
-import { arrayUnion, doc, Timestamp, updateDoc } from "firebase/firestore";
+import {
+  arrayUnion,
+  doc,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from "firebase/firestore";
 import React, { useContext, useState } from "react";
 import { FaImage } from "react-icons/fa";
 import { MdAttachFile } from "react-icons/md";
@@ -49,11 +55,35 @@ const Input = () => {
         }),
       });
     }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+        img,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+        img,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImg(null);
   };
 
   return (
     <div className="input">
-      <input type="text" placeholder="Type something..." />
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
       <div className="send">
         <MdAttachFile />
         <input
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -21,9 +21,9 @@ const Messages = () => {
   console.log(messages);
   return (
     <div className="messages">
-      {messages.map((m) => {
-        <Message message={m} key={m.id} />;
-      })}
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   );
 };
